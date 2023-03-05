da85e2f313a5c5dac485a243acb875e5de295200
stefanovidmar2005 <stefanovidmar@gmail.com>
2023-03-05 14:40:46 +0200

    Mobile Layout Completed

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,11 @@
 "use strict";
+
 // Selections
 const hamburger = document.querySelector(".hamburger");
 const navigationList = document.querySelector(".navigation__main-list");
 const menuIcon = document.querySelector(".menu-icon");
 const navbar = document.querySelector(".navigation");
+
 // Hamburget Functionality
 hamburger.addEventListener("click", function (e) {
   let target = e.currentTarget;
@@ -26,25 +28,21 @@ hamburger.addEventListener("click", function (e) {
     .querySelectorAll(".menu-icon")
     .forEach((icon) => (icon.src = "images/icon-arrow-down.svg"));
 });
-// Event Delegation on ul to find the target element that the li was clicked on
-// TODO: come up with a matching startegy we can use closest and classlists
+
+// Event Delegation
 navigationList.addEventListener("click", function (e) {
   const target = e.target.closest(".dropdown__list");
   if (!target) return;
-  // converting the html collection into an array so we can use the filter method
   const children = [...target.children];
   const icon = target.querySelector(".nav__links-flex").children[1];
-  // filtering out the children that only have the classlist of dropdown so we can select only the current dropdown upon each click so essentially this will be dynamic for other dropdown menus in future
   const dropdown = children.filter((child) => {
     return child.classList.contains("dropdown");
   });
   if (dropdown[0].classList.contains("dropdown-hidden")) {
     dropdown[0].classList.remove("dropdown-hidden");
-    // changing the icon src dynamically upon the closest in the nav__links-flex container
     icon.src = "images/icon-arrow-up.svg";
   } else {
     dropdown[0].classList.add("dropdown-hidden");
     icon.src = "images/icon-arrow-down.svg";
-    // edge case that the user may have had the dropdown open and then closed the menu therefore if he clicked again it woild be already open so to avoid this we will reset the dropdown when the user clicks on closing the menu
   }
 });
